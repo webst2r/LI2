2335a4627fcb835bf830ac8ecf74d74a09b28fb1
Leonardo Araújo Freitas <62023102+Leonardo1924@users.noreply.github.com>
2020-03-10 16:33:25 +0000

    Add files via upload

---
+++ Module1/cmake-build-debug/CMakeFiles/Module1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Module1.dir/camadadedados.c.o"
+  "CMakeFiles/Module1.dir/main.c.o"
+  "Module1"
+  "Module1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Module1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
