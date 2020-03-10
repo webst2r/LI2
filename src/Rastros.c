8d6f2efb7abb3b772dff22f251b6b616662bfbfb
leonardo <leonardo1924@protonmail.com>
2020-03-10 20:41:21 +0000

    Files modified

--- src/Rastros.c
+++ src/Rastros.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(){
+{
+ESTADO *e = inicializar_estado();
+interpretador(e);
+}
+return 0;
 }
--- src/interface.h
+++ src/interface.h
@@ -1,4 +1,6 @@
 #ifndef SRC_INTERFACE_H
 #define SRC_INTERFACE_H
 
+void mostrar_tabuleiro(ESTADO *e) {
+}
 #endif //SRC_INTERFACE_H
