1064aadf02420e9ba16eff789dcefeb09d26f56f
Rodrigo Pires Rodrigues <55801579+webst2r@users.noreply.github.com>
2020-03-22 18:20:53 -0100

    Add files via upload

--- src/interface.h
+++ src/interface.h
@@ -1,7 +1,21 @@
+/**
+@file interface.h
+Definição da interface e das funções que a manipulam
+*/
+
 #ifndef SRC_INTERFACE_H
 #define SRC_INTERFACE_H
 
+/**
+\brief Interpretador de jogadas
+*/
 int interpretador(ESTADO *e);
+
+/**
+\brief Função que mostra o tabuleiro
+*/
+
 void mostrar_tabuleiro(ESTADO *estado);
+
 
 #endif // SRC_INTERFACE_H
