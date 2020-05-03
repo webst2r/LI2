e41927b15acb68b7c4d3a1a815f2976c99e210a6
Rui Guilherme Bretão Rego dos Santos Monteiro <61871927+rushmetra@users.noreply.github.com>
2020-05-03 17:59:57 +0000

    Clean and tidy

--- projeto/camadadedados.c
+++ projeto/camadadedados.c
@@ -16,7 +16,6 @@ ESTADO *inicializar_estado() {
                           {VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO},
                           {VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, DOIS}},
             .ultima_jogada = {4,4},
-            .jogadas = {},
             .num_jogadas = 0,
             .jogador_atual = 1,
             .numeroComandos = 0,
@@ -27,28 +26,28 @@ ESTADO *inicializar_estado() {
 
 int obter_jogador_atual(ESTADO *estado) {
     int player;
-    player = estado->jogador_atual;
+    player = estado -> jogador_atual;
     return player;
 }
 
 int obter_numero_de_jogadas(ESTADO *estado) {
     int n_jogadas;
-    n_jogadas = estado->num_jogadas;
+    n_jogadas = estado -> num_jogadas;
     return n_jogadas;
 }
 
 CASA obter_estado_casa(ESTADO *estado, COORDENADA c) {
     CASA estadocasa;
-    estadocasa = estado->tab[c.linha][c.coluna];
+    estadocasa = estado -> tab [c.linha] [c.coluna];
     return estadocasa;
 }
 
 void atualiza_jogadas(ESTADO *e, COORDENADA c){
     if(obter_jogador_atual(e) == 1){
-        e->jogadas[obter_numero_de_jogadas(e)].jogador1 = c;
+        e -> jogadas[obter_numero_de_jogadas(e)].jogador1 = c;
     } else
-        e->jogadas[obter_numero_de_jogadas(e)].jogador2 = c;
-    e->ultima_jogada = c;
+        e -> jogadas[obter_numero_de_jogadas(e)].jogador2 = c;
+        e -> ultima_jogada = c;
 }
 
 void ler_aux(ESTADO *e, FILE *fp) {
@@ -56,48 +55,48 @@ void ler_aux(ESTADO *e, FILE *fp) {
     char buffer[BUF_SIZE];
     int line = 0;
     int w = 0;
-    e->num_jogadas = 0;
+    e -> num_jogadas = 0;
     while (w < 32) {
-        e->jogadas[w].jogador1.coluna = 0;
-        e->jogadas[w].jogador1.linha = 0;
-        e->jogadas[w].jogador2.coluna = 0;
-        e->jogadas[w].jogador2.linha = 0;
+        e -> jogadas[w].jogador1.coluna = 0;
+        e -> jogadas[w].jogador1.linha = 0;
+        e -> jogadas[w].jogador2.coluna = 0;
+        e -> jogadas[w].jogador2.linha = 0;
         w++;
     }
     int linha2, coluna2;
     for (linha2 = 0; linha2 < 8; linha2++) {
         for (coluna2 = 0; coluna2 < 8; coluna2++) {
             if (linha2 == 4 && coluna2 == 4)
-                e->tab[linha2][coluna2] = BRANCA;
+                e -> tab [linha2] [coluna2] = BRANCA;
             else
-                e->tab[linha2][coluna2] = VAZIO;
+                e -> tab [linha2] [coluna2] = VAZIO;
         }
     }
-    e->tab[0][0] = UM;
-    e->tab[7][7] = DOIS;
+    e -> tab[0][0] = UM;
+    e -> tab[7][7] = DOIS;
     while (fgets(buffer, BUF_SIZE, fp) != NULL) {
         if (line <= 7) {
             for (int j = 0; j <= 7; j++) {
                 if (buffer[j] == '.') e->tab[7 - line][j] = VAZIO;
-                else if (buffer[j] == '*') e->tab[7 - line][j] = BRANCA;
-                else if (buffer[j] == '#') e->tab[7 - line][j] = PRETA;
+                else if (buffer[j] == '*') e -> tab[7 - line] [j] = BRANCA;
+                else if (buffer[j] == '#') e -> tab[7 - line] [j] = PRETA;
             }
         } else {
             if (line >= 9) {
                 if (buffer[4] >= 'a' && buffer[4] <= 'h' && buffer[5] >= '1' && buffer[5] <= '8') {
-                    e->jogador_atual = 2;
+                    e -> jogador_atual = 2;
                     c1.linha = buffer[5] - '1';
                     c1.coluna = buffer[4] - 'a';
-                    e->jogadas[e->num_jogadas].jogador1 = c1;
-                    e->ultima_jogada = c1;
+                    e -> jogadas[e->num_jogadas].jogador1 = c1;
+                    e ->ultima_jogada = c1;
                 }
                 if (buffer[7] >= 'a' && buffer[7] <= 'h' && buffer[8] >= '1' && buffer[8] <= '8') {
-                    e->jogador_atual = 1;
+                    e -> jogador_atual = 1;
                     c2.linha = buffer[8] - '1';
                     c2.coluna = buffer[7] - 'a';
-                    e->jogadas[e->num_jogadas].jogador2 = c2;
-                    e->ultima_jogada = c2;
-                    e->num_jogadas++;
+                    e -> jogadas[e->num_jogadas].jogador2 = c2;
+                    e -> ultima_jogada = c2;
+                    e -> num_jogadas++;
                 }
             }
         }
@@ -113,22 +112,23 @@ void preta(ESTADO *e) {
         }
     }
 }
+
 void atualiza_estado(ESTADO *e) {
-    int w = e->numero_de_pos;
-    e->num_jogadas = w;
-    e->jogador_atual = 1;
+    int w = e -> numero_de_pos;
+    e -> num_jogadas = w;
+    e -> jogador_atual = 1;
 
     if(w == 0) {
-        e->ultima_jogada.coluna = 4;
-        e->ultima_jogada.linha = 4;
+        e -> ultima_jogada.coluna = 4;
+        e -> ultima_jogada.linha = 4;
     } else
-        e->ultima_jogada = e->jogadas[w-1].jogador2;
+        e -> ultima_jogada = e -> jogadas[w-1].jogador2;
 
     while (w < 32) {
-        e->jogadas[w].jogador1.coluna = 0;
-        e->jogadas[w].jogador1.linha = 0;
-        e->jogadas[w].jogador2.coluna = 0;
-        e->jogadas[w].jogador2.linha = 0;
+        e -> jogadas[w].jogador1.coluna = 0;
+        e -> jogadas[w].jogador1.linha = 0;
+        e -> jogadas[w].jogador2.coluna = 0;
+        e -> jogadas[w].jogador2.linha = 0;
         w++;
     }
     atualiza_tabuleiro(e);
@@ -136,91 +136,91 @@ void atualiza_estado(ESTADO *e) {
 
 void atualiza_tabuleiro(ESTADO *e) {
     int linha2, coluna2;
-    int pos = e->numero_de_pos;
+    int pos = e -> numero_de_pos;
     for (linha2 = 0; linha2 < 8; linha2++) {
         for (coluna2 = 0; coluna2 < 8; coluna2++) {
             if (linha2 == 4 && coluna2 == 4) {
                 if (pos == 0) {
-                    e->tab[linha2][coluna2] = BRANCA;
+                    e -> tab [linha2] [coluna2] = BRANCA;
                 } else {
-                    e->tab[linha2][coluna2] = PRETA;
+                    e -> tab [linha2] [coluna2] = PRETA;
                 }
             }
             else
-                e->tab[linha2][coluna2] = VAZIO;
+                e -> tab [linha2] [coluna2] = VAZIO;
         }
     }
-    e->tab[0][0] = UM;
-    e->tab[7][7] = DOIS;
+    e -> tab[0][0] = UM;
+    e -> tab[7][7] = DOIS;
 
     for(int i = 0; i < pos; i++) {
         COORDENADA c1, c2;
 
-        c1 =e->jogadas[i].jogador1;
-        c2 = e->jogadas[i].jogador2;
+        c1 = e -> jogadas[i].jogador1;
+        c2 = e -> jogadas[i].jogador2;
 
         if ((pos - 1) != i) {
-            e->tab[c1.linha][c1.coluna] = PRETA;
-            e->tab[c2.linha][c2.coluna] = PRETA;
+            e -> tab [c1.linha] [c1.coluna] = PRETA;
+            e -> tab [c2.linha] [c2.coluna] = PRETA;
         } else {
-            e->tab[c1.linha][c1.coluna] = PRETA;
-            e->tab[c2.linha][c2.coluna] = BRANCA;
+            e -> tab [c1.linha] [c1.coluna] = PRETA;
+            e -> tab [c2.linha] [c2.coluna] = BRANCA;
         }
     }
 }
 
 int obter_numero_de_pos(ESTADO *e) {
     int numero;
-    numero = e->numero_de_pos;
+    numero = e -> numero_de_pos;
     return numero;
 }
 
 void atualiza_jogador_atual(ESTADO *e) {
     if(obter_jogador_atual(e) == 1) {
-        e->jogador_atual = 2;
-    } else e->jogador_atual = 1;
+        e -> jogador_atual = 2;
+    } else e -> jogador_atual = 1;
 }
 
 int obter_numero_comandos(ESTADO *e) {
     int num_com = 0;
-    num_com = e->numeroComandos;
+    num_com = e -> numeroComandos;
     return num_com;
 }
 
 void maximiza_jogadas(ESTADO *e){
-    e->num_jogadas = 32;
+    e -> num_jogadas = 32;
 }
 
 void atualiza_num_jogadas(ESTADO *e) {
      int n = obter_numero_de_jogadas(e);
      if(obter_jogador_atual(e) == 2) {
          n++;
-     } e->num_jogadas = n;
+     } e -> num_jogadas = n;
 }
 
 void add_numerodecomandos(ESTADO *e) {
-    e->numeroComandos++;
+    e -> numeroComandos++;
 }
 
 void printMovs_aux(ESTADO *e, FILE *fp) {
     for (int i = 0; i < obter_numero_de_jogadas(e) + 1; i++) {
         if (i < 9) {
-            if (e->jogadas[i].jogador1.linha != 0 || e->jogadas[i].jogador1.coluna != 0) {
-                fprintf(fp, "0%d: %c%d", i + 1, e->jogadas[i].jogador1.coluna + 'a', e->jogadas[i].jogador1.linha + 1); // FIXME - ERRO DE LEITURA EXTRA DE UM MOVIMENTO ENCONTRA-SE NESTE PRIMEIRO IF
+            if (e -> jogadas[i].jogador1.linha != 0 || e -> jogadas[i].jogador1.coluna != 0) {
+                fprintf(fp, "0%d: %c%d", i + 1, e -> jogadas[i].jogador1.coluna + 'a', e -> jogadas[i].jogador1.linha + 1); 
             }
-            if (e->jogadas[i].jogador2.linha != 0 || e->jogadas[i].jogador2.coluna != 0)
-                fprintf(fp, " %c%d\n", e->jogadas[i].jogador2.coluna + 'a', e->jogadas[i].jogador2.linha + 1);
+            if (e -> jogadas[i].jogador2.linha != 0 || e -> jogadas[i].jogador2.coluna != 0)
+                fprintf(fp, " %c%d\n", e -> jogadas[i].jogador2.coluna + 'a', e -> jogadas[i].jogador2.linha + 1);
 
-            if ((e->jogadas[i].jogador1.linha != 0 || e->jogadas[i].jogador1.coluna != 0)&&
-                (e->jogadas[i].jogador2.coluna == 0 && e->jogadas[i].jogador2.linha == 0)) {
+            if ((e -> jogadas[i].jogador1.linha != 0 || e -> jogadas[i].jogador1.coluna != 0)&&
+                (e -> jogadas[i].jogador2.coluna == 0 && e -> jogadas[i].jogador2.linha == 0)) {
                 fprintf(fp, "\n");
             }
         } else {
-            if (e->jogadas[i].jogador1.linha != 0 || e->jogadas[i].jogador1.coluna != 0)
-                fprintf(fp, "%d: %c%d", i + 1, e->jogadas[i].jogador1.coluna + 'a', e->jogadas[i].jogador1.linha + 1);
+            if (e -> jogadas[i].jogador1.linha != 0 || e -> jogadas[i].jogador1.coluna != 0)
+                fprintf(fp, "%d: %c%d", i + 1, e -> jogadas[i].jogador1.coluna + 'a', e -> jogadas[i].jogador1.linha + 1);
 
-            if (e->jogadas[i].jogador2.linha != 0 || e->jogadas[i].jogador2.coluna != 0)
-                fprintf(fp, " %c%d\n", e->jogadas[i].jogador2.coluna + 'a', e->jogadas[i].jogador2.linha + 1);
+            if (e -> jogadas[i].jogador2.linha != 0 || e -> jogadas[i].jogador2.coluna != 0)
+                fprintf(fp, " %c%d\n", e -> jogadas[i].jogador2.coluna + 'a', e -> jogadas[i].jogador2.linha + 1);
             else
                 fprintf(fp, "\n");
         }
